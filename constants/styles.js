2d7cf21b7a2cc44df8b02b9260f3a8b84ba8f1bd
Houssam almomani <houssamalmomani1@gmail.com>
2022-10-07 14:50:03 +0300

    complete Authentication app using firebase

---
+++ constants/styles.js
@@ -0,0 +1,7 @@
+export const Colors = {
+    primary100: 'white',
+    primary500: '#05445E',
+    primary800: '#189AB4',
+    error100: '#F1CED4',
+    error500: '#DB1F48',
+}
---
+++ screens/LoginScreen.js
@@ -0,0 +1,37 @@
+import { useContext, useState } from 'react';
+import { Alert } from 'react-native';
+
+import AuthContent from '../components/Auth/AuthContent';
+import LoadingOverlay from '../components/ui/LoadingOverlay';
+import { AuthContext } from '../store/auth-context';
+import { login } from '../util/auth'
+
+function LoginScreen() {
+    const [isAuthentication, setIsAuthentication] = useState(false);
+
+    const authCtx = useContext(AuthContext);
+
+    async function loginHandler({ email, password }) {
+        setIsAuthentication(true);
+        try {
+
+            const token = await login(email, password);
+            authCtx.authenticate(token);
+        } catch (error) {
+            Alert.alert(
+                'Authentication failed!',
+                'Could not log you in. Please check your credentials or try again later!'
+            );
+            setIsAuthentication(false);
+        }
+
+    }
+
+    if (isAuthentication) {
+        return <LoadingOverlay message={"Logging you in..."} />
+    }
+
+    return <AuthContent isLogin onAuthenticate={loginHandler} />;
+}
+
+export default LoginScreen;
